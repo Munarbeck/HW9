4a85ac06fd9c920c7f06cc4f2418ef1e3d70880b
Munarbeck <93381204+Munarbeck@users.noreply.github.com>
2021-12-05 12:24:53 +0600

    Initial commit

---
+++ src/com/company/Cat.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public final class Cat extends Animal {
+    private String name;
+    private String breed;
+    private String owner;
+    public Cat(String name,String breed,Color color){
+        this.name = name;
+        this.breed = breed;
+        super.setColor(color);
+    }
+    public Cat(String name,String breed,Color color,String owner){
+        this.name = name;
+        this.breed = breed;
+        this.owner = owner;
+        super.setColor(color);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+    public String getInfo(){
+        return super.getInfo() +
+                "\nName: " + name+
+                "\nBreed: "+ breed;
+    }
+    public String getIInfo(){
+        return super.getInfo() +
+                "\nName: " + name+
+                "\nBreed: "+ breed+
+                "\nOwner: "+owner;
+    }
+}
---
+++ src/com/company/Dog.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Dog extends Animal {
+    private String name;
+    private String breed;
+    private String owner;
+    public Dog(String name, String breed, Color color) {
+        this.name = name;
+        this.breed = breed;
+        super.setColor(color);
+    }
+//    public Dog( String breed,String name,Color color,String owner) {
+//        this.name = name;
+//        this.breed = breed;
+//        this.owner = owner;
+//      super.setColor(color);
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getInfo() {
+        return super.getInfo() +
+                "\nName: " + name +
+                "\nBreed: " + breed;
+    }
+}
